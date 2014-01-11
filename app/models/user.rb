66d9d6a435a74d67efa4481dbc8423dae624ab5c
Spencer Eldred <eldredspencer@gmail.com>
2014-01-11 05:52:42 -0800

    Fix profile update - remove uniqueness requirement on email

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   validates :zipcode, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   validates :email, presence: true
   validates :phone, presence: true
