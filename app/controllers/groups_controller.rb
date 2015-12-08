a2c86843bf4fc39f2301b93ba3b35708c8e1e300
Spencer Eldred <eldredspencer@gmail.com>
2015-12-08 06:35:06 -1000

    Show group to admin, show all groups to super_admin

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,9 +3,17 @@ class GroupsController < ApplicationController
   before_filter :authorize_admin
 
   def index
+    if current_user.function == 'super_admin'
+      groups = Group.all
+    else
+      groups = Group.where(id: @current_profile.group_id)
+    end
+    groups.each do |group|
+      group[:member_count] = group.users.length
+    end
     respond_to do |format|
       format.html
-      format.json { render :json => Group.all }
+      format.json { render :json => groups }
     end
   end
 
--- db/migrate/20131023190055_create_transactions.rb
+++ db/migrate/20131023190055_create_transactions.rb
@@ -1,17 +1,17 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
-      t.integer       :recycler_user_id
-      t.integer       :redeemer_user_id
+      t.integer        :recycler_user_id
+      t.integer        :redeemer_user_id
       t.string         :plastic
       t.string         :glass
       t.string         :cans
       t.string         :other
-      t.datetime    :selection_date
-      t.datetime    :completion_date
-      t.integer       :rating
-      t.float           :latitude
-      t.float           :longitude
+      t.datetime       :selection_date
+      t.datetime       :completion_date
+      t.integer        :rating
+      t.float          :latitude
+      t.float          :longitude
       t.string         :address
       t.string         :city
       t.string         :state
